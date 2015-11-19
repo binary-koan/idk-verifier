b4dcd6789b1a840b3a22d9286eb6d262250a22b7
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-19 15:51:05 +1300

    Added examples and modified 'main.rb' to dump expressions

--- lib/main.rb
+++ lib/main.rb
@@ -3,3 +3,15 @@
 
 require_relative 'parser/token'
 require_relative 'parser/parser'
+
+FILENAME = ARGV[0]
+
+puts "Opening '#{FILENAME}'"
+
+code = File.read(FILENAME)
+parser = Parser.new(code)
+
+loop do
+  expr = parser.parse_expression
+  puts "Expression: #{expr}"
+end
