05aa288aa9dea463534e7904b0697ac071961228
Jono Mingard <reason.koan@gmail.com>
2015-11-19 13:22:30 +1300

    Add comparisons and arithmetic to range

--- lib/verifier/range.rb
+++ lib/verifier/range.rb
@@ -1,17 +1,89 @@
 module Verifier
-  class Range
+  class ValueRange
     include Enumerable
 
     attr_reader :upper
     attr_reader :lower
 
-    def initialize(upper, lower)
-      @upper = upper
-      @lower = lower
+    def initialize(first_end, second_end)
+      @lower, @upper = [first_end, second_end].minmax
     end
 
     def each
       (upper..lower).each { |i| yield i }
+    end
+
+    def range
+      upper - lower
+    end
+
+    # Comparisons
+
+    def <(other)
+      self <= (other - 1)
+    end
+
+    def <=(other)
+      if other.is_a?(ValueRange)
+        upper <= other.lower
+      else
+        upper <= other
+      end
+    end
+
+    def >(other)
+      self >= (other + 1)
+    end
+
+    def >=(other)
+      if other.is_a?(ValueRange)
+        lower >= other.upper
+      else
+        lower >= other
+      end
+    end
+
+    def ==(other)
+      if other.is_a?(ValueRange)
+        range == 0 && other.range == 0 && lower == other.lower
+      else
+        range == 0 && lower == other
+      end
+    end
+
+    def !=(other)
+      outside?(other)
+    end
+
+    def outside?(other)
+      upper < other.lower || lower > other.upper
+    end
+
+    # Arithmetic
+
+    def -@
+      # This is probably wrong ...
+      ValueRange.new(-lower, -upper)
+    end
+
+    [:+, :-, :*, :/].each do |operator|
+      define_method(operator) do |other|
+        if other.is_a?(ValueRange)
+          ValueRange.new(lower.send(operator, other.lower), upper.send(operator, other.upper))
+        else
+          ValueRange.new(lower.send(operator, other), upper.send(operator, other))
+        end
+      end
+    end
+
+    # Utilities
+
+    def to_s
+      "<ValueRange #{lower}..#{upper}>"
+    end
+
+    def to_range
+      lower..upper
     end
   end
 end
---
+++ spec/verifier/range_spec.rb
@@ -0,0 +1,137 @@
+require "spec_helper"
+
+RSpec.describe Verifier::ValueRange do
+  let(:range1) { 0..100 }
+  let(:range2) { 0..100 }
+
+  let(:value_range1) { Verifier::ValueRange.new(range1.begin, range1.end) }
+  let(:value_range2) { Verifier::ValueRange.new(range2.end, range2.begin) }
+
+  describe "#==" do
+    subject { value_range1 == value_range2 }
+
+    context "when the bounds are identical but the range is non-zero" do
+      it { is_expected.to eq false }
+    end
+
+    context "when the bounds are identical and the range is zero" do
+      let(:range1) { 1..1 }
+      let(:range2) { 1..1 }
+      it { is_expected.to eq true }
+    end
+
+    context "when the bounds are different" do
+      let(:range1) { 1..2 }
+      let(:range2) { 0..1 }
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe "#<" do
+    subject { value_range1 < value_range2 }
+
+    context "when the first range is completely below the second one" do
+      let(:range1) { 1..5 }
+      let(:range2) { 6..10 }
+      it { is_expected.to eq true }
+    end
+
+    context "when the ranges overlap" do
+      let(:range1) { 1..5 }
+      let(:range2) { 4..10 }
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe "#>" do
+    subject { value_range1 > value_range2 }
+
+    context "when the first range is completely above the second one" do
+      let(:range1) { 6..10 }
+      let(:range2) { 1..5 }
+      it { is_expected.to eq true }
+    end
+
+    context "when the ranges overlap" do
+      let(:range1) { 4..10 }
+      let(:range2) { 1..5 }
+      it { is_expected.to eq false }
+    end
+  end
+
+  describe "#+" do
+    subject { (value_range1 + value_range2).to_range }
+
+    context "with two positive ranges" do
+      let(:range1) { 1..4 }
+      let(:range2) { 2..8 }
+      it { is_expected.to eq 3..12 }
+    end
+
+    context "with a positive and negative range" do
+      let(:range1) { 1..5 }
+      let(:range2) { -10..-1 }
+      it { is_expected.to eq -9..4 }
+    end
+
+    context "with two negative ranges" do
+      let(:range1) { -5..-1 }
+      let(:range2) { -10..-5 }
+      it { is_expected.to eq -15..-6 }
+    end
+  end
+
+  describe "#-" do
+    subject { (value_range1 - value_range2).to_range }
+
+    context "with two positive ranges" do
+      let(:range1) { 2..8 }
+      let(:range2) { 1..4 }
+      it { is_expected.to eq 1..4 }
+    end
+
+    context "with a positive and negative range" do
+      let(:range1) { 1..5 }
+      let(:range2) { -10..-1 }
+      it { is_expected.to eq 2..15 }
+    end
+
+    context "with two negative ranges" do
+      let(:range1) { -5..-1 }
+      let(:range2) { -10..-5 }
+      it { is_expected.to eq 0..9 }
+    end
+  end
+
+  describe "#*" do
+    subject { (value_range1 * value_range2).to_range }
+
+    context "with two positive ranges" do
+      let(:range1) { 1..4 }
+      let(:range2) { 2..8 }
+      it { is_expected.to eq 2..32 }
+    end
+
+    context "with a positive and negative range" do
+      let(:range1) { 1..5 }
+      let(:range2) { -10..-1 }
+      it { is_expected.to eq -50..-1 }
+    end
+
+    context "with two negative ranges" do
+      let(:range1) { -5..-1 }
+      let(:range2) { -10..-5 }
+      it { is_expected.to eq 5..50 }
+    end
+  end
+
+  describe "#/" do
+    subject { (value_range1 / value_range2).to_range }
+
+    context "with two positive ranges" do
+      let(:range1) { 4..8 }
+      let(:range2) { 1..4 }
+      it { is_expected.to eq 1..8 }
+    end
+  end
+end
