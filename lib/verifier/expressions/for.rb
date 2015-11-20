74862cdab0b868cd32398d0631094037404ee1f3
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-20 15:17:23 +1300

    Add 'ForExpression'

---
+++ lib/verifier/expressions/for.rb
@@ -0,0 +1,32 @@
+module Verifier
+
+  # A C style for-loop.
+  #
+  # for (begin; end; step) {
+  #     body
+  # }
+  class ForExpression
+
+    attr_reader :begin, :end, :step, :body
+
+    def new(begin_expr, end_cond,
+            step, body)
+      @begin = begin_expr
+      @end = end_cond
+      @step = step
+      @body = body
+    end
+
+    def ==(other)
+      if other.is_a?(ForExpression)
+        @begin == other.begin &&
+        @end == other.end &&
+        @step == other.step
+        @body == other.body
+      else
+        false
+      end
+    end
+
+  end
+end
