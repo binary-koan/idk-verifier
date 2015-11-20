c3a92778eba91bdd21d4ba9a48fe4b75adae2f17
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-20 14:34:39 +1300

    Added 'If' expression type

---
+++ lib/verifier/expressions/if.rb
@@ -0,0 +1,34 @@
+module Verifier
+
+  # A single branch in an if-elsif-else block.
+  #
+  # This consists of a condition and a body.
+  # Else blocks have no condition (nil).
+  class IfBranch
+
+    attr_reader :condition, :body
+
+    def initialize(condition, body)
+      @condition = condition
+      @body = body
+    end
+
+    def is_else
+      # Else blocks have no condition
+      @condition == nil
+    end
+  end
+
+  class IfExpression
+    include Expression
+
+    attr_reader :branches
+
+    def initialize(branches)
+      @branches = branches
+
+      # TODO: Make sure there is only one else
+      # (unconditional) branch.
+    end
+  end
+end
